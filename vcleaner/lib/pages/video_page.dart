828fbe4b6eaa87126bfa774b25ec5577b168a159
Gliitchhh410 <ahmedmounir532@gmail.com>
2023-12-07 00:30:13 +0200

    Return Arrow for the Second Page.

--- vcleaner/lib/pages/video_page.dart
+++ vcleaner/lib/pages/video_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class VideoPage extends StatelessWidget {
-  const VideoPage({super.key});
+  const VideoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+
+        iconTheme: IconThemeData(
+          color: Colors.grey[800], // Set the color of the back button icon
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
     );
   }
 }
